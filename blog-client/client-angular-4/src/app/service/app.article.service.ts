fe1f60d6cf4149f6bf1ec95ad57a1475b37243d3
Lycsona <lycsona@gmail.com>
2017-08-14 21:12:49 +0300

    Add getArticleByUrlId method

--- blog-client/client-angular-4/src/app/service/app.article.service.ts
+++ blog-client/client-angular-4/src/app/service/app.article.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-import { Observable } from 'rxjs';
-import { apiPrefix, CommonUtil } from '../utils/common.util';
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import {apiPrefix, CommonUtil} from '../utils/common.util';
 
 const ARTICLE = apiPrefix.concat('/api/articles');
 
@@ -13,7 +13,16 @@ export class AppArticleService {
 
     public getArticles(): Observable<Response> {
         return this.http
-            .get(ARTICLE,  CommonUtil.getContentTypeJson())
+            .get(ARTICLE, CommonUtil.getContentTypeJson())
+            .map((res: Response) => {
+                return res;
+            })
+            .catch(CommonUtil.handleError);
+    }
+
+    public getArticleByUrlId(id: string): Observable<Response> {
+        return this.http
+            .get(ARTICLE.concat('/').concat(id), null)
             .map((res: Response) => {
                 return res;
             })
