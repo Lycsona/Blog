96602c6b1cd660a70d2ac54683b7523f7cd121ee
Lycsona <lycsona@gmail.com>
2017-08-16 20:59:17 +0300

    Create form

---
+++ blog-server/src/App/BlogBundle/Form/TagType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\BlogBundle\Form;
+
+use App\BlogBundle\Entity\Tag;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * Defines the form used to create and manipulate Tag.
+ */
+class TagType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name');
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Tag::class,
+            'csrf_protection' => false,
+        ]);
+    }
+}
