d1d7fc7c4a235aff45bcc7b44c944e1cb7334738
Lycsona <lycsona@gmail.com>
2017-08-16 20:23:08 +0300

    Update article fixture, add order for fixtures.

--- blog-server/src/App/BlogBundle/DataFixtures/ORM/ArticleFixtures/ArticleFixtures.php
+++ blog-server/src/App/BlogBundle/DataFixtures/ORM/ArticleFixtures/ArticleFixtures.php
@@ -3,17 +3,18 @@
 namespace App\BlogBundle\DataFixtures\ORM;
 
 use App\BlogBundle\Entity\Article;
+use App\BlogBundle\Entity\Tag;
 use Doctrine\Common\DataFixtures\AbstractFixture;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 
 /**
  * Class ArticleFixtures
  *
  * @package App\BlogBundle\DataFixtures\ORM
  */
-class ArticleFixtures extends AbstractFixture implements ContainerAwareInterface
+class ArticleFixtures extends AbstractFixture implements OrderedFixtureInterface
 {
     use ContainerAwareTrait;
 
@@ -22,10 +23,10 @@ class ArticleFixtures extends AbstractFixture implements ContainerAwareInterface
      */
     public function load(ObjectManager $manager)
     {
-        $Article = new Article();
+        $article = new Article();
 
-        $Article->setName('SAN ANTONIO');
-        $Article->setContent(
+        $article->setName('SAN ANTONIO');
+        $article->setContent(
             'SAN ANTONIO — If within the past few years you received a package,
              roamed a shopping mall, boarded a plane, train, ferry or cruise ship,
               went to a major sporting event, ran a marathon, attended a concert,
@@ -40,8 +41,19 @@ class ArticleFixtures extends AbstractFixture implements ContainerAwareInterface
                  Despite decades of trying, researchers have yet to develop a machine 
                  as exquisitely sensitive and discerning as a dog’s nose. Nor can a robot
                   rove with the agility and ease of a dog.');
+        $article->addTag($this->getReference('tag-test'));
 
-        $manager->persist($Article);
+        $manager->persist($article);
         $manager->flush();
+    }
+
+    /**
+     * Get the order of this fixture
+     *
+     * @return integer
+     */
+    function getOrder()
+    {
+        return 2;
     }
 }
