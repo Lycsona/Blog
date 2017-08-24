0bde393fad847e85ae2d63cfe17bcaa21989366a
Lycsona <lycsona@gmail.com>
2017-08-24 17:53:27 +0300

    Add createdAt and updatedAt, tags properties and getters&setters in Tag DTO.

---
+++ blog-client/client-angular-4/src/app/dto/TagDto.ts
@@ -0,0 +1,56 @@
+export class TagDto {
+
+    private _id?: number;
+    private _createdAt?: Date;
+    private _updatedAt?: Date;
+    private _name?: string;
+
+    constructor() {
+        this._id = 0;
+        this._createdAt = new Date;
+        this._updatedAt = new Date;
+        this._name = "";
+        this._cssSelector = "";
+    }
+
+    get id(): number {
+        return this._id;
+    }
+
+    set id(value: number) {
+        this._id = value;
+    }
+
+    get createdAt(): Date {
+        return this._createAt;
+    }
+
+    set createdAt(value: Date) {
+        this._createAt = value;
+    }
+
+    get updatedAt(): Date {
+        return this._updatedAt;
+    }
+
+    set updatedAt(value: Date) {
+        this._updatedAt = value;
+    }
+
+    get name(): string {
+        return this._name;
+    }
+
+    set name(value: string) {
+        this._name = value;
+    }
+
+    public toJSON() {
+        return {
+            id: this._id,
+            createAt: this._createdAt,
+            updatedAt: this._updatedAt,
+            name: this._name
+        };
+    }
+}
