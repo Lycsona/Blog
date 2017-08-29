6430688b0a2906453446c5404112a9f2a5b94d71
Lycsona <lycsona@gmail.com>
2017-08-29 15:37:55 +0300

    Create method selectAllArticlesByTag with query.

--- blog-server/src/App/BlogBundle/Repository/ArticleRepository.php
+++ blog-server/src/App/BlogBundle/Repository/ArticleRepository.php
@@ -2,8 +2,25 @@
 
 namespace App\BlogBundle\Repository;
 
+use App\BlogBundle\Entity\Article;
+use App\BlogBundle\Entity\Tag;
 use Doctrine\ORM\EntityRepository;
 
 class ArticleRepository extends EntityRepository
 {
+    /**
+     * @param $tag
+     * @return array
+     */
+    public function selectAllArticlesByTag($tag)
+    {
+        $builder = $this->getEntityManager()->createQueryBuilder()
+            ->select('a.name, a.content, a.createdAt, a.updatedAt, t.name as tag')
+            ->from(Article::class, 'a')
+            ->innerJoin(Tag::class, 't')
+            ->where(':tag MEMBER OF a.tags')
+            ->setParameter('tag', $tag);
+
+        return $builder->getQuery()->getResult();
+    }
 }
