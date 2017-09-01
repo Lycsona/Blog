08d945b6095acc2d029135644ee15c0c15b6d488
Lycsona <lycsona@gmail.com>
2017-09-01 17:29:52 +0300

    Create AppPageViewService

---
+++ blog-client/client-angular-4/src/app/service/app.page.view.service.ts
@@ -0,0 +1,21 @@
+import {Injectable} from '@angular/core';
+import {Http, Response} from '@angular/http';
+import {Observable} from 'rxjs';
+import {apiPrefix, CommonUtil} from '../util/common.util';
+
+const PAGE_VIEW = apiPrefix.concat('/api/page-views');
+
+@Injectable()
+export class AppPageViewService {
+
+    constructor(private http: Http) {
+    }
+
+    public incrementPageView(id: string): Observable<Response> {
+        return this.http.post(PAGE_VIEW.concat('/').concat(id), null, CommonUtil.getContentTypeJson())
+            .map((res: Response) => {
+                return res;
+            })
+            .catch(CommonUtil.handleError);
+    }
+}
