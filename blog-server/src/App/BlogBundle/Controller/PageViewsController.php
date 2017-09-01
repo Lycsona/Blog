0cb15b31cb4139f7ecf309dfac4b0f014068ca05
Lycsona <lycsona@gmail.com>
2017-09-01 17:29:35 +0300

    Update incrementPageViews method

--- blog-server/src/App/BlogBundle/Controller/PageViewsController.php
+++ blog-server/src/App/BlogBundle/Controller/PageViewsController.php
@@ -8,17 +8,41 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class PageViewsController extends Controller
 {
-    private function incrementPageViews(Article $article)
+    /**
+     * Increment PageViews.
+     *
+     * @ApiDoc(
+     *   section = "PageViews",
+     *   resource = true
+     * )
+     *
+     * @Method("POST")
+     * @Route("api/page-views/{id}")
+     * @param integer $id
+     *
+     * @return JsonResponse
+     */
+    public function incrementPageViews($id)
     {
         $entityManager = $this->getDoctrine()->getManager();
+        $article = $entityManager->getRepository(Article::class)->find($id);
 
         $pageViews = $article->getPageViews();
         $pageViews->setCounter($pageViews->getCounter() + 1);
 
         $entityManager->persist($pageViews);
         $entityManager->flush();
+
+        $pageViewsJson = $this->get('serializer')->serialize(
+            $pageViews,
+            'json'
+        );
+
+        return JsonResponse::fromJsonString($pageViewsJson, Response::HTTP_OK);
     }
 }
