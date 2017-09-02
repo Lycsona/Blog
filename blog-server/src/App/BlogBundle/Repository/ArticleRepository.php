f12bf3acb3edc7c75267b8ba82586423c206fac6
Lycsona <lycsona@gmail.com>
2017-09-02 23:18:51 +0300

    Create pagination

--- blog-server/src/App/BlogBundle/Repository/ArticleRepository.php
+++ blog-server/src/App/BlogBundle/Repository/ArticleRepository.php
@@ -7,7 +7,37 @@ use Doctrine\ORM\EntityRepository;
 class ArticleRepository extends EntityRepository
 {
     /**
-     * Get all articles by tag id.
+     * Get all articles with pagination.
+     *
+     * @param string $page
+     * @param string $size
+     * @return array
+     */
+    public function selectAllArticles($page, $size)
+    {
+        $builder = $this->getEntityManager()->createQueryBuilder()
+            ->select('a')
+            ->from($this->getEntityName(), 'a');
+
+        $query = $builder->getQuery()
+            ->setFirstResult($size * $page)
+            ->setMaxResults($size)
+            ->getResult();
+
+        $totalPages = ceil($this->getArticlesTotalCount() / $size);
+        $firstPage = 1 ? $page == 0 : 0;
+        $lastPage = 1 ? $page == $totalPages : 0;
+
+        return [
+            'queryResult' => $query,
+            'totalPages' => $totalPages,
+            'firstPage' => $firstPage,
+            'lastPage' => $lastPage
+        ];
+    }
+
+    /**
+     * Get all articles by tag id with pagination.
      *
      * @param integer $tag
      * @return array
@@ -23,5 +53,20 @@ class ArticleRepository extends EntityRepository
             ->setParameter('tag', $tag);
 
         return $builder->getQuery()->getResult();
+    }
+
+    /**
+     * Get total count of articles
+     *
+     * @return int
+     */
+    public function getArticlesTotalCount()
+    {
+        $query = $this->getEntityManager()->createQueryBuilder();
+
+        $query->select('count(a.id)')
+            ->from($this->getEntityName(), 'a');
+
+        return (int)$query->getQuery()->getSingleScalarResult();
     }
 }
