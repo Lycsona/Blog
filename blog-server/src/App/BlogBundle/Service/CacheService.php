63798e951433d87e280a75bcecfcc5f818cbb9d8
Lycsona <lycsona@gmail.com>
2017-09-25 13:53:35 +0300

    Implement caching tags for articles and deleting them.

--- blog-server/src/App/BlogBundle/Service/CacheService.php
+++ blog-server/src/App/BlogBundle/Service/CacheService.php
@@ -6,11 +6,18 @@ interface CacheService
 {
     public function getAllArticles();
 
+    public function getArticlesWithTags($articles);
+
+    public function getArticlesTagsKey($articles);
+
     public function saveArticles($data);
 
-    public function deleteArticles();
+    public function saveArticleTags($data);
 
     public function hasCache($key);
 
     public function getValue($key);
-}
+
+    public function deleteArticles();
+
+    public function deleteArticlesTags($articles);}
--- blog-server/src/App/BlogBundle/Service/Impl/CacheServiceImpl.php
+++ blog-server/src/App/BlogBundle/Service/Impl/CacheServiceImpl.php
@@ -26,7 +26,30 @@ class CacheServiceImpl implements CacheService
 
     public function getAllArticles()
     {
+        $this->deleteArticles();
+
         return $this->cache->getItem('articles');
+    }
+
+    public function getArticlesWithTags($articles)
+    {
+        foreach ($articles as $article) {
+            $tags = $this->getValue($this->cache->getItem('article_tags_' . $article->getId()));
+
+            $article->setTags($tags);
+        }
+    }
+
+    public function getArticlesTagsKey($articles)
+    {
+        $tags = [];
+
+        foreach ($articles as $article) {
+
+            $tags[] = 'article_tags_' . $article->getId();
+        }
+
+        return $tags;
     }
 
     public function saveArticles($articles)
@@ -36,10 +59,32 @@ class CacheServiceImpl implements CacheService
         $cachedArticles->set($articles);
 
         $this->cache->save($cachedArticles);
+
+        $this->saveArticleTags($articles);
+    }
+
+    public function saveArticleTags($articles)
+    {
+        foreach ($articles as $article) {
+
+            $articlesTag = $this->cache->getItem('article_tags_' . $article->getId());
+
+            $articlesTag->set($article->getTags()[0]);
+
+            $this->cache->save($articlesTag);
+        }
     }
 
     public function deleteArticles()
     {
         $this->cache->deleteItem('articles');
+    }
+
+    public function deleteArticlesTags($articles)
+    {
+        $articlesTags = $this->getArticlesTagsKey($articles);
+        foreach ($articlesTags as $tagKey) {
+            $this->cache->deleteItem($tagKey);
+        }
     }
 }
