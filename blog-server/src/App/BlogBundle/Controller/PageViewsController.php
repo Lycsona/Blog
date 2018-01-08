3daf0669889b7c4866e5052255c8a7d2d8bf48c1
Lycsona <lycsona@gmail.com>
2018-01-08 13:09:46 +0200

    create page views controller

--- blog-server/src/App/BlogBundle/Controller/PageViewsController.php
+++ blog-server/src/App/BlogBundle/Controller/PageViewsController.php
@@ -2,14 +2,11 @@
 
 namespace App\BlogBundle\Controller;
 
-use App\BlogBundle\Entity\Article;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class PageViewsController extends Controller
 {
@@ -29,20 +26,6 @@ class PageViewsController extends Controller
      */
     public function incrementPageViews($id)
     {
-        $entityManager = $this->getDoctrine()->getManager();
-        $article = $entityManager->getRepository(Article::class)->find($id);
-
-        $pageViews = $article->getPageViews();
-        $pageViews->setCounter($pageViews->getCounter() + 1);
-
-        $entityManager->persist($pageViews);
-        $entityManager->flush();
-
-        $pageViewsJson = $this->get('serializer')->serialize(
-            $pageViews,
-            'json'
-        );
-
-        return JsonResponse::fromJsonString($pageViewsJson, Response::HTTP_OK);
+        return $this->get('page_views')->incrementPageViews($id);
     }
 }
