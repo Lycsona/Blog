bbbe3d757d1195565796ae47668fdb65ac5da018
Lycsona <lycsona@gmail.com>
2018-01-08 13:06:47 +0200

    add StompService, add publish

--- blog-client/client-angular-4/src/app/article/article.component.ts
+++ blog-client/client-angular-4/src/app/article/article.component.ts
@@ -1,10 +1,11 @@
 import {Component, Inject, OnInit} from "@angular/core";
 import {AppArticleService} from "../service/app.article.service";
-import {AppPageViewService} from "../service/app.page.view.service";
 import {ActivatedRoute} from "@angular/router";
 import {ArticleDto} from "../dto/ArticleDto";
 import {DOCUMENT} from "@angular/platform-browser";
 import {CommonUtil} from "../util/common.util";
+import {Message} from '@stomp/stompjs';
+import {StompService} from '@stomp/ng2-stompjs';
 
 @Component({
     selector: 'article',
@@ -14,15 +15,23 @@ export class ArticleComponent implements OnInit {
 
     public article: ArticleDto;
     public originalArticle: ArticleDto;
+    stomp_subscription: any;
 
     constructor(private appArticleService: AppArticleService, private route: ActivatedRoute,
-                private appPageViewService: AppPageViewService, @Inject(DOCUMENT) private document: any) {
+                @Inject(DOCUMENT) private document: any, private _stompService: StompService) {
         this.article = new ArticleDto();
         this.originalArticle = new ArticleDto();
     }
 
     public ngOnInit() {
         this.getArticle();
+
+        // this.stomp_subscription.map((message: Message) => {
+        //
+        //     return message.body;
+        // }).publish((msg_body: string) => {
+        //     console.log(`Received: ${msg_body}`);
+        // });
     }
 
     private getArticle() {
@@ -43,7 +52,9 @@ export class ArticleComponent implements OnInit {
                     this.article.updatedAt = jsonArticle.updatedAt;
                     this.article.tags = jsonArticle.tags;
 
-                    this.appPageViewService.incrementPageView(<string> jsonArticle.id).subscribe();
+                    this.stomp_subscription = this._stompService.publish('/queue/page-views',
+                        <string> jsonArticle.id);
+
                 }, CommonUtil.handleError)
         }).catch((error) => console.error(error));
     }
