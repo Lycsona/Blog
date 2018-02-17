20c197c3299fbc0e860839033c725446301c1c8e
Lycsona <lycsona@gmail.com>
2018-02-17 18:04:49 +0200

    create FileUploaderImpl service

---
+++ blog-server/src/App/BlogBundle/Service/Impl/FileUploaderImpl.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\BlogBundle\Service\Impl;
+
+use App\BlogBundle\Service\FileUploader;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+class FileUploaderImpl implements FileUploader
+{
+    private $targetDir;
+
+    public function __construct($targetDir)
+    {
+        $this->targetDir = $targetDir;
+    }
+
+    public function upload(UploadedFile $file)
+    {
+        $fileName = md5(uniqid()) . '.' . $file->guessExtension();
+
+        $file->move($this->getTargetDir(), $fileName);
+
+        return $fileName;
+    }
+
+    public function getTargetDir()
+    {
+        return $this->targetDir;
+    }
+}
