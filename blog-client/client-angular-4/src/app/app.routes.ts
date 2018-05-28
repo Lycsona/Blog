629beb4304b4eb26d15f44979b369bf7f2f03696
Maria <maria.ostashevskaya@appfor.de>
2018-05-28 19:10:13 +0300

    add admin component to route

--- blog-client/client-angular-4/src/app/app.routes.ts
+++ blog-client/client-angular-4/src/app/app.routes.ts
@@ -6,6 +6,7 @@ import {ArticleComponent} from "./article/article.component";
 import {ListOfArticles} from "./list-of-article/list-of-articles.component";
 import {AuthGuard} from "./util/auth-guard";
 import {LoginComponent} from "./login/login.component";
+import {AdminComponent} from "./+admin/admin.component";
 
 export const ROUTES: Routes = [
     {path: '', component: HomeComponent},
